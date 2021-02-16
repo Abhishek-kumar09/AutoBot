1b612827982c84e42a449c39446ba907fb966827
Abhishek-kumar09 <abhimait1909@gmail.com>
2021-02-16 15:24:46 +0530

    Corrected css links

--- src/views/Homepage/Home.js
+++ src/views/Homepage/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "../../components/Header/Header";
-import "./Home.css";
+import "./Home.scss";
 import heroBg from "../../assets/img/hero-bg.gif";
 
 export default function Home() {
