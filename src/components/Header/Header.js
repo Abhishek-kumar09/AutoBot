86480ec596496a7d2dd3d7655b53cc6efef8685b
petercrackthecode <nguyenvandung02051999@gmail.com>
2021-02-22 17:39:18 -0800

    Building the landing page

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,34 +1,45 @@
 import React, { useState } from "react";
-import { Dialog, Grid, DialogTitle, DialogContent, Typography, Button } from "@material-ui/core";
+import {
+  Dialog,
+  Grid,
+  DialogTitle,
+  DialogContent,
+  Typography,
+  Button,
+} from "@material-ui/core";
 import TextField from "@material-ui/core/TextField";
 import SearchIcon from "@material-ui/icons/Search";
-import './header.scss'
-import { signInWithGoogle, logout } from '../../firebase'
+import "./header.scss";
+import { signInWithGoogle, logout } from "../../firebase";
 
 export default function Header({ user }) {
   const [openDialog, setopenDialog] = useState(false);
 
   const handleClose = () => {
-    setopenDialog(false)
-  }
+    setopenDialog(false);
+  };
 
   const handleGoogleAuth = () => {
-    signInWithGoogle().then(result => {
-      console.log(result)
-      setopenDialog(false)
-    }).catch(e => {
-      console.log('error', e)
-    })
-  }
+    signInWithGoogle()
+      .then((result) => {
+        console.log(result);
+        setopenDialog(false);
+      })
+      .catch((e) => {
+        console.log("error", e);
+      });
+  };
 
   const handleLogout = () => {
-    logout().then(()=> {
-      console.log("logout Successfull")
-      setopenDialog(false)
-    }).catch((e) => {
-      console.log('error', e)
-    })
-  }
+    logout()
+      .then(() => {
+        console.log("logout Successfull");
+        setopenDialog(false);
+      })
+      .catch((e) => {
+        console.log("error", e);
+      });
+  };
 
   return (
     <div id="header">
@@ -43,7 +54,7 @@ export default function Header({ user }) {
         <Grid item style={{ width: "90%" }}>
           <TextField
             id="input-with-icon-grid"
-            label="Which database are you looking for?"
+            label="Which dataset are you looking for?"
             style={{ width: "100%" }}
           />
         </Grid>
@@ -52,26 +63,45 @@ export default function Header({ user }) {
         </Grid>
       </Grid>
       <div className="auth-btn">
-        <button className="block round" onClick={() => { setopenDialog(true) }}>{user ? user.displayName : 'Login'}</button>
+        <button
+          className="block round"
+          onClick={() => {
+            setopenDialog(true);
+          }}
+        >
+          {user ? user.displayName : "Login"}
+        </button>
         <button className="block round accent">Sign up</button>
       </div>
 
-      <Dialog maxWidth={'xs'} onClose={handleClose} open={openDialog} style={{ textAlign: 'center' }}>
+      <Dialog
+        maxWidth={"xs"}
+        onClose={handleClose}
+        open={openDialog}
+        style={{ textAlign: "center" }}
+      >
         <DialogTitle id="simple-dialog-title">Login to AutoBot</DialogTitle>
         <DialogContent className="dialog-content">
+          <Typography>Your destination for all the ML queries</Typography>
           <Typography>
-            Your destination for all the ML queries
-          </Typography>
-          <Typography>
-            Stop searching for your desired dataset and start building immediately.
-            We provide the data on your preferences.
+            Stop searching for your desired dataset and start building
+            immediately. We provide the data on your preferences.
           </Typography>
         </DialogContent>
         <DialogContent className="dialog-content">
-          {user
-            ? <Button className="block round" style={{padding: '8px 24px'}} onClick={handleLogout}>Logout</Button>
-            : <Button className="block round accent" onClick={handleGoogleAuth}>Google Sign In</Button>
-          }
+          {user ? (
+            <Button
+              className="block round"
+              style={{ padding: "8px 24px" }}
+              onClick={handleLogout}
+            >
+              Logout
+            </Button>
+          ) : (
+            <Button className="block round accent" onClick={handleGoogleAuth}>
+              Google Sign In
+            </Button>
+          )}
         </DialogContent>
       </Dialog>
     </div>
